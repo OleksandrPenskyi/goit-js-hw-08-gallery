c2eedaa0600955b4747fc9538a61bd0547e0eafe
OleksandrPenskyi <suchgoodidea@gmail.com>
2021-02-05 10:23:38 +0200

    fixed methods

--- task-gallery.js
+++ task-gallery.js
@@ -7,6 +7,7 @@ const closeModalBtnRef = document.querySelector('button[data-action="close-light
 const overlayClickRef = document.querySelector('.lightbox__overlay'); // Ссылка на серый фон (оверлей)
 
 const listOfLinks = pics.map(pic => pic.original); // массив ссылок на оригиналбные (увеличенные) картинки
+let activeIdx; // текущий индекс "таргетной" текущей картинки (счетчик для переключения по кликам влево / вправо)
 
 // Перебираем массив pics для получения строки-li с нужными атрибутами для создания html динамически
 const listOfimg = pics.reduce((accum, pic, idx) => {
@@ -33,68 +34,66 @@ const listOfimg = pics.reduce((accum, pic, idx) => {
 ulListRef.insertAdjacentHTML('afterbegin', listOfimg);
 
 ulListRef.addEventListener('click', onOpenModal); // Открытие модалки по клику на img
-closeModalBtnRef.addEventListener('click', onCloseModal); // Закрытие модалки по клику на кнопку
-overlayClickRef.addEventListener('click', onCloseModal); // Закрытие модалки по клику на Оверлей
+closeModalBtnRef.addEventListener('click', onHandleToPressSomeBtns); // Закрытие модалки по клику на кнопку
+overlayClickRef.addEventListener('click', onHandleToPressSomeBtns); // Закрытие модалки по клику на Оверлей
 
-//! ф-я открытия модалки
+// !ф-я открытия модалки
 function onOpenModal(event) {
   event.preventDefault(); // запрет браузеру на стандартные действия, чтобы не переходил по ссылке кликнув на pic
   const target = event.target;
 
-  // добавляем class="is-open" для бэкдропа
   if (target.nodeName === 'IMG') {
     // добавление класса для открытия бэкдропа
     backDropRef.classList.add('is-open');
     // добавляем слушателя на закрытие бэкдропа по Escape
-    window.addEventListener('keydown', onCloseModal);
+    window.addEventListener('keydown', onHandleToPressSomeBtns);
     // Подмена значения src и alt у <img>
     originalImgRef.src = target.dataset.source;
     originalImgRef.alt = target.alt;
   }
 
-  let activeIdx = +event.target.getAttribute('data-idx'); // таргет на клацнутом єлементе, то-есть на img
+  activeIdx = +event.target.getAttribute('data-idx'); // таргет на клацнутом єлементе, то-есть на img
   // console.log(activeIdx);
-  galleryListing(activeIdx);
 }
-// !ф-я перелистывания модалки
-function galleryListing(activeIdx) {
-  window.addEventListener('keydown', event => {
-    if (event.code === 'ArrowRight') {
-      activeIdx += 1;
-      if (activeIdx < 9) {
-        // console.log('Наклацанный индекс:', activeIdx);
-        originalImgRef.src = listOfLinks[`${activeIdx}`];
-      } else {
-        activeIdx = listOfLinks.length - 1;
-      }
-    }
 
-    if (event.code === 'ArrowLeft') {
-      activeIdx -= 1;
-      if (activeIdx >= 0) {
-        // console.log('Наклацанный индекс:', activeIdx);
-        originalImgRef.src = listOfLinks[`${activeIdx}`];
-      } else {
-        activeIdx = 0;
-      }
-    }
-  });
-}
-// !общий метод для закрытия модалки
-function onCloseModal(event) {
+// !универсальный метод по кликам кнопок
+function onHandleToPressSomeBtns(event) {
   // закрытие по ескейпу или клику на оверлей
   if (event.code === 'Escape' || event.target === event.currentTarget) {
+    // console.log('закрыл какой-то кнопкой');
     closeModal();
-    clearPicAttributes();
+  }
+
+  // перелистывание по клику на Right button
+  if (event.code === 'ArrowRight') {
+    activeIdx += 1;
+    if (activeIdx < 9) {
+      // console.log('Наклацанный индекс:', activeIdx);
+      originalImgRef.src = listOfLinks[`${activeIdx}`];
+    } else {
+      activeIdx = listOfLinks.length - 1;
+    }
+  }
+
+  // перелистывание по клику на Left button
+  if (event.code === 'ArrowLeft') {
+    activeIdx -= 1;
+    if (activeIdx >= 0) {
+      // console.log('Наклацанный индекс:', activeIdx);
+      originalImgRef.src = listOfLinks[`${activeIdx}`];
+    } else {
+      activeIdx = 0;
+    }
   }
 }
-// !ф-я закрытия модалки
+
+// !функция закрытия модалки и очистки атрибутов
 function closeModal() {
+  // закрытие модалки
   backDropRef.classList.remove('is-open');
-  window.removeEventListener('keydown', onCloseModal);
-}
-// !ф-я очистки значения атрибута src элемента img.lightbox__image
-function clearPicAttributes() {
+  // снятие слушателя с ESC
+  window.removeEventListener('keydown', onHandleToPressSomeBtns);
+  // очистка значения атрибута src элемента img.lightbox__image
   originalImgRef.src = '';
   originalImgRef.alt = '';
 }
